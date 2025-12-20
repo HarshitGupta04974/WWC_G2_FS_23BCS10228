94de85d8e38065ac4f1aca2dc9be414055a65043
Harshit Gupta <harshit.412.gupta@gmail.com>
2025-12-20 11:17:48 +0530

    Added the new spring api

---
+++ TASK 3/wwc_api/src/test/java/com/wwc/WwcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwcApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
